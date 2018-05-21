93d58f0143b4a2ede06d33d43bea89bd3287e4ea
wfy815775 <815775754@qq.com>
2018-05-21 14:23:38 +0800

    主要是并发练习

---
+++ src/parallelPattern/StampedLockDemo/LongAccuDemo.java
@@ -0,0 +1,30 @@
+package parallelPattern.StampedLockDemo;
+
+import java.util.Random;
+import java.util.concurrent.atomic.LongAccumulator;
+
+public class LongAccuDemo {
+    public static void main(String[] args) {
+        LongAccumulator accumulator = new LongAccumulator(Long::max ,Long.MIN_VALUE);
+        Thread[] ts = new Thread[1000];
+
+        for (int i = 0; i < 1000; i++) {
+            ts[i] = new Thread(() -> {
+                Random random = new Random();
+                long value = random.nextLong();
+                accumulator.accumulate(value);
+            });
+            ts[i].start();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            try {
+                ts[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(accumulator.longValue());
+    }
+}
